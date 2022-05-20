5de216012cf690ad61401153aa8c85a0504149d5
Brice Pirioua <brice.pirioua@gmail.com>
2022-05-20 11:20:20 +0200

    lists bookmarks

--- db/seeds.rb
+++ db/seeds.rb
@@ -9,6 +9,9 @@
 require 'json'
 require "open-uri"
 
-url = "http://tmdb.lewagon.com/movie/top_rated?api_key=<your_api_key>"
+url = "https://tmdb.lewagon.com/movie/top_rated"
 user_serialized = URI.open(url).read
-user = JSON.parse(user_serialized)
+movies = JSON.parse(user_serialized)
+movies["results"].each do |movie|
+Movie.create(title: movie["original_title"], overview: movie["overview"], poster_url: "https://image.tmdb.org/t/p/w500/#{movie['poster_path']}", rating: movie["vote_average"])
+end
